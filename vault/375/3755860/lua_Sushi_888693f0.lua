31f4f32afc594e1213cb8337fd226e7ee2ceab67
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 10:07:26 -0300

    Upload Lote 1 - Prefixo 375

---
+++ vault/375/3755860/lua_Sushi_888693f0.lua
@@ -0,0 +1,11 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(3755860) -- BLACK SOULS
+-- MAIN APP DEPOTS
+addappid(3755863, 1, "284e12d82c8696e2c3d4fd500c0da44ddca49794c6ccd0682e0ec75b18ba43d7") -- Depot 3755863
+setManifestid(3755863, "5145146131198925425", 814840268)
