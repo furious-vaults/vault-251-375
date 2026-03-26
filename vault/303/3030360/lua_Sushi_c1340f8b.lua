7c9b0b3889abcef6093e5a5ceae07ff8a3309ec4
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:15:00 -0300

    Upload Lote 1 - Prefixo 303

---
+++ vault/303/3030360/lua_Sushi_c1340f8b.lua
@@ -0,0 +1,7 @@
+-- Made By @F1uxin
+-- MAIN APPLICATION
+addappid(3030360) -- Puppet Team
+
+-- MAIN APP DEPOTS
+addappid(3030362, 1, "7030120465b819a52103de650970f5cc2e1da5bc3bc4ceb9246eba6b5e63f5a7") -- Game Content (Linux Binaries)
+setManifestid(3030362, "3539976087241675074", 0)
