bb8d589541902ee5ff531a27ada2e9a43dc0a5d4
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:21:02 -0300

    Upload Lote 1 - Prefixo 311

---
+++ vault/311/3110010/lua_Sushi_5c7fbe88.lua
@@ -0,0 +1,17 @@
+-- Made by @f1uxin, If you would like to check out my discord server here's a link : https://discord.gg/NmmpgnAgen
+-- In my server we help with mani & lua files, fixes, and honestly just anything about piracy,
+-- You can dm me "f1uxin" on discord if you have any questions or anything I'm normally pretty active, and enjoy!
+-- If you want to redistribute/share these files please add and dm "f1uxin" on discord to talk more, me and my friend pay for these games with our own money, we do get games cheaper but its still money that we have and are spending so it sucks when people steal/don't credit me at all.
+
+-- P.S On some socials you may find another person with a close name to me, its "f1uxin sells" or "f1uxins shop" any accounts like that are not mine, i do not sell anything, everything I show is free.
+
+-- P.S. #2. This one is important, you see the "setManifestid" lines below right? If you want your game to "auto update" put "--" without the "" Infront of each "setManifestid" or you can completely remove those lines but that is not recommended, then just save this file, and add it however you add mani & lua files. Example "--setManifestid"
+
+-- Guns.lol link : https://guns.lol/f1uxin
+
+
+-- MAIN APPLICATION 
+
+addappid(3110010)
+addappid(3110011,0,"d03f6986d839d4e4985f1e72ca84c58a1964a311e1064b28bc5426d3a2e4a66a")
+setManifestid(3110011,"8084553989613768341")
---
+++ vault/311/3114900/lua_Sushi_b83cca27.lua
@@ -0,0 +1,16 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(3114900) -- DREAD FLATS
+
+-- MAIN APP DEPOTS
+addappid(3114901, 1, "edf860798dc72b4e2772f638655e92816bb02c130ff60b5a2c2fe460e48f2b45") -- Depot 3114901
+setManifestid(3114901, "3337312410117176250", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
