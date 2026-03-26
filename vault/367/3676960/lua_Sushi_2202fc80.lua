f55cdc242f28c0ec0e9f1ce2357a3b82b2ec1e90
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 10:04:12 -0300

    Upload Lote 1 - Prefixo 367

---
+++ vault/367/3676960/lua_Sushi_2202fc80.lua
@@ -0,0 +1,7 @@
+-- Made by f1uxin, please read the read me TXT file
+-- MAIN APPLICATION
+addappid(3676960) -- The Last Train
+
+-- MAIN APP DEPOTS
+addappid(3676961, 1, "dbe396514ce3fc8be3577f1f52fa9aaafae776560598070c625c0c4878fae30a") -- Depot 3676961
+setManifestid(3676961, "4779868834228722503", 0)
