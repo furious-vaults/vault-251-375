e21dda82438b730605b34b5211941d02cf3b68be
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:23:59 -0300

    Upload Lote 1 - Prefixo 257

---
+++ vault/257/2570210/lua_Sushi_a1fbf913.lua
@@ -0,0 +1,16 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2570210) -- Eden Crafters
+-- MAIN APP DEPOTS
+addappid(2570211, 1, "a6d9fd6c7cb73b8737246837d4b5446f3af752492a3a7adec43c60b3c590ca34") -- Depot 2570211
+setManifestid(2570211, "4195167969874160573", 4430263678)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
