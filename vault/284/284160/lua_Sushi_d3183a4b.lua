94e9d17e98d756cdc5b30db7f7e7cbc27a323054
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:53:02 -0300

    Upload Lote 1 - Prefixo 284

---
+++ vault/284/284160/lua_Sushi_d3183a4b.lua
@@ -0,0 +1,18 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(284160) -- BeamNG.drive
+
+-- MAIN APP DEPOTS
+addappid(284161, 1, "0e77f1cd7bb3169d9885cc26c7d2ef9fffc02c8c49671bec7b7308dd6cf86229") -- BeamNG.drive Windows
+setManifestid(284161, "7602390841611719634", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
---
+++ vault/284/2843190/lua_Ryuu_e70862c6.lua
@@ -0,0 +1,3 @@
+addappid(2843190)
+addappid(2843191,0,"fba4bbaff05d9c2d954a673410b3ed2468e54afa6403fe20bacfdddd06d0bb0d")
+setManifestid(2843191,"4344000381318624650")
