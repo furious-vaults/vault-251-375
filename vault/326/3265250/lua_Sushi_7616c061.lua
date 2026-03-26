f6edc8330ff97aed3c1dd6ca63a8cd86620d458b
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:36:16 -0300

    Upload Lote 1 - Prefixo 326

---
+++ vault/326/3265250/lua_Sushi_7616c061.lua
@@ -0,0 +1,13 @@
+-- Made By @F1uxin
+-- MAIN APPLICATION
+addappid(3265250) -- MindsEye
+
+-- MAIN APP DEPOTS
+addappid(3265251, 1, "4314da2fbb6b1038e0f8199b23f2f2c4ab77a7b1fb15289036bdc4fb0178a0a1") -- Main Game Content (Windows Content)
+setManifestid(3265251, "3452716186547003133", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
