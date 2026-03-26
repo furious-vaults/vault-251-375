632ff6eb0385699ea92cff424b00cf7388856e27
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:32:22 -0300

    Upload Lote 2 - Prefixo 320

---
+++ vault/320/3207400/lua_Sushi_58802d1e.lua
@@ -0,0 +1,12 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(3207400) -- Silent Still
+
+-- MAIN APP DEPOTS
+addappid(3207401, 1, "f88661c7db00151d3013dfae8455a2c17edd9ae56c55fa0c48c70f09ef366aa0") -- Main Game Content (Windows Content)
+setManifestid(3207401, "201631557037086589", 0)
---
+++ vault/320/3207670/lua_Sushi_0137c0a7.lua
@@ -0,0 +1,10 @@
+-- Made by f1uxin, please read the read me TXT file.
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+addappid(3207670) 
+addappid(3207671, 1, "2d0e5a8969b201ba636cc318891c091f3590e610318b5df540c2be4d5e4a66ff")
+setManifestid(3207671, "5325082571257344027", 0)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") 
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") 
+setManifestid(228990, "1829726630299308803", 0)
