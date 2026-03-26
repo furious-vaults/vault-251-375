432597cf4c70ac007b31a3db09bbf780706a719b
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:25:03 -0300

    Upload Lote 1 - Prefixo 316

---
+++ vault/316/3164500/lua_Sushi_86261029.lua
@@ -0,0 +1,7 @@
+-- Made By @F1uxin
+-- MAIN APPLICATION
+addappid(3164500) -- Schedule I
+
+-- MAIN APP DEPOTS
+addappid(3164501, 1, "81d0de07788de885787b0519b9dabf51618be6cba9124497decf572ac96624f4") -- Main Game Content (Windows Content)
+setManifestid(3164501, "4917957940192610936", 0)
