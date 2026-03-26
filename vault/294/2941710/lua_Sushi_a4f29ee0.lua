f197d3e39d5de5f3a96c6a7311d2164e4ac651ed
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:03:33 -0300

    Upload Lote 1 - Prefixo 294

---
+++ vault/294/2941710/lua_Sushi_a4f29ee0.lua
@@ -0,0 +1,9 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+addappid(2941710)
+addappid(2941711,0,"58f3fb42e32fc7f54e16d87c3952b2a1c537e0be679fd35a1d16acc5e4d870d4")
+setManifestid(2941711,"1047705544686681197")
