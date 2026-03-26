c30ef58f999e6e6fa343587d3d18813638926653
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:13:47 -0300

    Upload Lote 2 - Prefixo 301

---
+++ vault/301/3016920/lua_Ryuu_91e34f86.lua
@@ -0,0 +1,8 @@
+addappid(3016920)
+addappid(3296930)
+addappid(3296940)
+addappid(3296950)
+addappid(3347980)
+addappid(3453170)
+addappid(3016921,0,"3f651ffe3e62f40c3a3359eeff2e7251707fe9367bbce65df51e287273255fad")
+setManifestid(3016921,"2612151924108508079")
