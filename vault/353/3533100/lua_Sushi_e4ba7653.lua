ac1881b127b8be88f3e6b501a5fe410d586016ce
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:56:45 -0300

    Upload Lote 1 - Prefixo 353

---
+++ vault/353/3533100/lua_Sushi_e4ba7653.lua
@@ -0,0 +1,18 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(3533100) -- Wrap House Simulator🌯
+
+-- MAIN APP DEPOTS
+addappid(3533101, 1, "165e0dadd51768e263a833318ade634f7f7a76a7fcf9d32356eb4a03e64f0f90") -- Depot 3533101
+setManifestid(3533101, "2117817651888525705", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
