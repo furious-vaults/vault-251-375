efafa0a2282d0f4d1fca6da10979f2115f4dd327
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:10:17 -0300

    Upload Lote 2 - Prefixo 298

---
+++ vault/298/2989950/lua_Sushi_73c01e7e.lua
@@ -0,0 +1,10 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(2989950) -- The Bathhouse | 地獄銭湯 Restored Ver.
+
+-- MAIN APP DEPOTS
+addappid(2989951, 1, "46ba9c35893709885a253dc16ae624a59f7ba30e7d5673c8d70bab12dd2dfa75") -- Main Game Content (Windows Content)
+setManifestid(2989951, "8502982591506204150", 0)
