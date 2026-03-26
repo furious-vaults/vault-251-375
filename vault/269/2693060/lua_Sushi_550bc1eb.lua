8612a3041da4ced1747a4ae5f8788582093dccef
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:35:50 -0300

    Upload Lote 1 - Prefixo 269

---
+++ vault/269/2693060/lua_Sushi_550bc1eb.lua
@@ -0,0 +1,18 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2693060) -- Garten of Banban 7
+
+-- MAIN APP DEPOTS
+addappid(2693061, 1, "da1139079f0e4ea6d14ac29edba3e6860898e0a523c799af51248a060db5bcc8") -- Main Game Content (Windows Content)
+setManifestid(2693061, "5491537789826049552", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
