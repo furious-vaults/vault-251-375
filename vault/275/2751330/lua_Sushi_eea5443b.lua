2f4c1b216033ce8a3828a63bf2d5c58bb3baa8ae
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:42:35 -0300

    Upload Lote 1 - Prefixo 275

---
+++ vault/275/2751330/lua_Sushi_eea5443b.lua
@@ -0,0 +1,23 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2751330) -- Helix Jump
+
+-- MAIN APP DEPOTS
+addappid(2751331, 1, "9311d12434a65f0fb1089412277bde4cefb56f9e38dc9ba441a43cde71fee310") -- Main Game Content (Windows Content)
+setManifestid(2751331, "2154077787273698452", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Helix Jump Letter Madness (AppID: 2828070)
+addappid(2828070)
+addappid(2828070, 1, "14b9748f87964b5959dd2ce2442da85eb843e4bd246ed584a55c0d7926f6ef60") -- Helix Jump Letter Madness - Main Content
+setManifestid(2828070, "1004469991290073738", 0)
+
+-- Helix Jump Party Skins (AppID: 2828080)
+addappid(2828080)
+addappid(2828080, 1, "b60fcba88966bfc3ec84dd5a5d9a31a38e55530263d92733eae88262779fa09d") -- Helix Jump Party Skins - Main Content
+setManifestid(2828080, "5310072092295361206", 0)
---
+++ vault/275/2753900/lua_Sushi_ea7d87ad.lua
@@ -0,0 +1,16 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(2753900) -- The King is Watching
+-- MAIN APP DEPOTS
+addappid(2753901, 1, "416d49c23e9d45fbd519129ae945ff52e8e57b2e8a0170688a8623c6ca0d3941") -- Depot 2753901
+setManifestid(2753901, "2865728548311595139", 345357842)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
