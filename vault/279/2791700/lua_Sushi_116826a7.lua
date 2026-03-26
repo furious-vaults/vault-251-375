1065436ac9c9d0d779db482960711a7c7486cc9e
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:46:46 -0300

    Upload Lote 1 - Prefixo 279

---
+++ vault/279/2791700/lua_Sushi_116826a7.lua
@@ -0,0 +1,12 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2791700) -- Infantry Attack
+
+-- MAIN APP DEPOTS
+addappid(2791701, 1, "12642b33843bc523f6a5daf518c326c5ac04348b5a10db889642e67e9e16c0b8") -- Main Game Content (Windows Content)
+setManifestid(2791701, "4649323198611796670", 0)
---
+++ vault/279/2796020/lua_Sushi_caa99c8f.lua
@@ -0,0 +1,10 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(2796020) -- Refuted Wind
+
+-- MAIN APP DEPOTS
+addappid(2796021, 1, "8f6e62f99eeef44ea2a35b8075a08cc007d6506f5a5c1c3034846e866588289c") -- Main Game Content (Windows Content)
+setManifestid(2796021, "7564205257771334440", 0)
