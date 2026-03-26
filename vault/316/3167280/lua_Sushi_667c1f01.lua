86b2afe590cab1eb2f99b777fd5377d267ca248e
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:25:28 -0300

    Upload Lote 2 - Prefixo 316

---
+++ vault/316/3167280/lua_Sushi_667c1f01.lua
@@ -0,0 +1,11 @@
+-- This file was fetched from twentytwo cloud which is the exclusive property of Twentytwo server.
+-- Redistribution of twentytwo's files is not allowed AT ALL
+-- Join the server here: https://discord.com/invite/vwGWeTFTXW
+
+addappid(3167280)
+addappid(3167281,0,"1b1929846ec645ef5eb073427b69f02c9876da76886a8e07b53365296d852d77")
+setManifestid(3167281,"310406201677295171")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+setManifestid(228989, "3514306556860204959", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+setManifestid(228990, "1829726630299308803", 0)
---
+++ vault/316/3169520/lua_Sushi_3f348df0.lua
@@ -0,0 +1,14 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(3169520) -- Warhammer 40,000: Space Marine - Master Crafted Edition
+-- MAIN APP DEPOTS
+addappid(3169521, 1, "9c327e47da8ad83eb864ec73bad3a78e2f7ff2844fd4cc6c3ed89e72725e9217") -- Depot 3169521
+setManifestid(3169521, "2036309795747341059", 46817411779)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
