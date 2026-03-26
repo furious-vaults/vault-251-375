ce09e9adee08ceace125451a55e79234afdf008c
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:21:55 -0300

    Upload Lote 1 - Prefixo 254

---
+++ vault/254/2542120/lua_Sushi_56efdda1.lua
@@ -0,0 +1,11 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2542120) -- NINJA GAIDEN: Ragebound
+-- MAIN APP DEPOTS
+addappid(2542121, 1, "ddd21357db67ee08b9fe146fe995760876a9fd89db8fd7e4389a982f7d28cf14") -- Depot 2542121
+setManifestid(2542121, "1398797921565314396", 2680668528)
