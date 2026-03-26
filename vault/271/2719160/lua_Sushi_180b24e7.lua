0cbbc8d688f1abf264127bbcf542c561b71cc404
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:37:50 -0300

    Upload Lote 2 - Prefixo 271

---
+++ vault/271/2719160/lua_Sushi_180b24e7.lua
@@ -0,0 +1,20 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(2719160) -- Contractors Showdown
+
+-- MAIN APP DEPOTS
+addappid(2719161, 1, "09414e3bc70d3b13b3e1145978229a49a56414f7aec9ed4eedc8598d31d720b6") -- Depot 2719161
+setManifestid(2719161, "1783298438347147128", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3682230) -- Contractors Showdown  ExfilZone - NTG Standard Issue
+addappid(3682240) -- Contractors Showdown  ExfilZone - Johnnys secret stash
