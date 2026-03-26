e77c376ba5c6582f7232c6502a2d3ec3574deffc
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:46:45 -0300

    Upload Lote 1 - Prefixo 339

---
+++ vault/339/3397800/lua_Sushi_fbb270b8.lua
@@ -0,0 +1,16 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(3397800) -- vROV Pilot: TITANIC
+
+-- MAIN APP DEPOTS
+addappid(3397801, 1, "1d458eda842f24025de40e7ac58195dd6a50d3b96d5ead560b42a7610703234d") -- Depot 3397801
+setManifestid(3397801, "252330685836753572", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
