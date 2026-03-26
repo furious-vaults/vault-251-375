b21903d3706f52570144b964e74b17bba131b850
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:54:17 -0300

    Upload Lote 2 - Prefixo 285

---
+++ vault/285/2858370/lua_Sushi_bc2222f5.lua
@@ -0,0 +1,16 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2858370, 1, "e66cd6fc2bb280106b437148a5abd541dbf520076daa7cfc606f7d117dac35ce") -- Voxel Project VR
+-- MAIN APP DEPOTS
+addappid(2858371, 1, "0ba2a43eddd2720ce19d754de3ec5aedd2f850bb29ad1d61ffa4cedde17e0258") -- Depot 2858371
+setManifestid(2858371, "7078762030911294754", 11438116315)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
