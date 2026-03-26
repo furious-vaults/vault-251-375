52aa582e7368e5a3a86d5b7753fd1f8af9c5533d
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 09:01:53 -0300

    Upload Lote 2 - Prefixo 292

---
+++ vault/292/2928600/lua_Sushi_add0d194.lua
@@ -0,0 +1,23 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy / https://discord.gg/m8PxjTwrnv
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(2928600) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2
+-- MAIN APP DEPOTS
+addappid(2928601, 1, "486448df6cd95d0d663ae6dbc0dc5cb7ed590875a2a7481fee25ffe7572b60fc") -- Depot 2928601
+setManifestid(2928601, "6544946417668235170", 25974484063)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3371870) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Battle Attire Tanjiros Kimono (Entertainment District), Inosukes Kimono (Entertainment District), and Uzuis Shinobi Attire
+addappid(3371880) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Character Unlock Keys Sanemi Shinazugawa, Obanai Iguro, and Gyomei Himejima
+addappid(3371890) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 VS Mode System Voice Upper Rank Demons Set (Akaza, Daki, Gyutaro, Gyokko, Zohakuten)
+addappid(3371900) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Character Unlock Keys Mitsuri Kanroji and Muichiro Tokito
+addappid(3371910) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Digital Version Bonus - Character Unlock Keys Academy Rengoku and Academy Uzui
+addappid(3900330) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 2 Core Add-on Bundle
