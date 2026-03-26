e4d2bc71b1a38415a0cd6b0c62269272f4566aa5
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 10:06:44 -0300

    Upload Lote 1 - Prefixo 373

---
+++ vault/373/3732190/lua_Sushi_5150661a.lua
@@ -0,0 +1,6 @@
+-- Made by f1uxin, please read the read me TXT file.
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+addappid(3732190)
+addappid(3732191, 1, "736c8ca12b2ea7547c93293dd11e3ba673ba3a49b5052787df317298a4058795") 
+setManifestid(3732191, "8913964692630183469", 0)
