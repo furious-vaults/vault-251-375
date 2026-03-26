66d75faafb06545edb2253b60f7cb44ae8ca96d3
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:35:23 -0300

    Upload Lote 2 - Prefixo 268

---
+++ vault/268/2686350/lua_Sushi_8223b878.lua
@@ -0,0 +1,12 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @F1uxin on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!)
+
+-- MAIN APPLICATION
+addappid(2686350) -- Home Defender
+
+-- MAIN APP DEPOTS
+addappid(2686351, 1, "823e79cdbedf3d30d10707b7e055640b151a50667581d6288034495da767a562") -- Depot 2686351
+setManifestid(2686351, "8050615440243445645", 0)
