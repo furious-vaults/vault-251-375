cc1f962756b436993f06bf574f5b32713e5ea5e8
furious-vaults <diegohenriquewcl@hotmail.com>
2026-03-26 08:50:52 -0300

    Upload Lote 2 - Prefixo 282

---
+++ vault/282/2826800/lua_Sushi_16ef8498.lua
@@ -0,0 +1,15 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(2826800) -- Amanda the Adventurer 2
+
+-- MAIN APP DEPOTS
+addappid(2826801, 1, "e0d7d9f87ac41ba6feaea2ac279cd90853990f9df6fccb2308e58d5a0eae1533") -- Main Game Content (Windows Content)
+setManifestid(2826801, "1928045169186532536", 0)
+
+-- DLCS EXCLUDED (NO DEPOT KEYS, NO DEDICATED DEPOTS)
+-- addappid(3392190) -- Amanda the Adventurer 2 Soundtrack (no keys available)
